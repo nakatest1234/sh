8df564615a34926b55a0ca7583dea1ce027829a5
nakatest1234 <ambient2step@gmail.com>
2016-04-28 17:08:50 +0900

    Create auto_ip2dns.sh

---
+++ aws/auto_ip2dns.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+ZONEID=$1
+PUBLIC_HOST=$2
+
+TMPL='{
+  "HostedZoneId": "{%ZONEID%}",
+  "ChangeBatch": {
+    "Changes": [
+      {
+        "Action": "UPSERT",
+        "ResourceRecordSet": {
+        "Name": "{%HOST%}",
+        "ResourceRecords": [
+          {
+            "Value": "{%IP%}"
+          }
+        ],
+        "TTL": 300,
+        "Type": "A"
+        }
+      }
+    ]
+  }
+}'
+
+InstanceID=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/instance-id`
+IP=`/usr/bin/curl -s http://169.254.169.254/latest/meta-data/public-ipv4`
+
+JSON=`echo ${TMPL} | sed -e "s/{%ZONEID%}/${ZONEID}/g;s/{%IP%}/${IP}/g;s/{%HOST%}/${PUBLIC_HOST}/g;"`
+
+aws route53 change-resource-record-sets --cli-input-json "${JSON}"
